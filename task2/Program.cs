31267353405820a12690d5521cf569ba54e7d72b
Евгений <Grekos_89@mail.ru>
2023-01-18 16:17:15 +0300

    задача3

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дни недели
+Console.WriteLine("введите число");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+    System.Console.WriteLine("noy");
+    break;
+    case 2:
+    System.Console.WriteLine("noy");
+    break;
+    case 3:
+    System.Console.WriteLine("noy");
+    break;
+    case 4:
+    System.Console.WriteLine("noy");
+    break;
+    case 5:
+    System.Console.WriteLine("noy");
+    break;
+    case 6:
+    System.Console.WriteLine("yes");
+    break;
+    case 7:
+    System.Console.WriteLine("yes");
+    break;
+}
