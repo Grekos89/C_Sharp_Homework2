50593eb63b410c253c6123c36e9577bf6377790e
Евгений <Grekos_89@mail.ru>
2023-01-18 15:53:55 +0300

    задача1

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = (num - ((num / 100) * 100) - (num % 10)) / 10;
+
+System.Console.WriteLine(result);
+
