cdb7fb4be7a0ccdb6e29207fd7c0e1b8b7f800b8
Евгений <Grekos_89@mail.ru>
2023-01-19 15:39:23 +0300

    задача2

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// вывести третью цифру задонного числа
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+// while(num != 0)
+// {
+//    num = num / 10;
+//    count++;
+// }
+// System.Console.WriteLine(count); 
+// Смог только узнать длинну числа через деление, а дальше я не могу придумать как решить
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string text = Convert.ToString(num);
+
+if (text.Length > 2)
+{
+   Console.WriteLine(text[2]);
+}
+else {
+   Console.WriteLine("нет");
+}
