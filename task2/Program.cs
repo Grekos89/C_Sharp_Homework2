38fee435a6c281370f54cd498aab29d720a7015d
Евгений <Grekos_89@mail.ru>
2023-01-20 12:13:51 +0300

    изменения

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,27 +2,36 @@
 Console.WriteLine("введите число");
 int day = Convert.ToInt32(Console.ReadLine());
 
-switch (day)
+// switch (day)
+// {
+//     case 1:
+//     System.Console.WriteLine("noy");
+//     break;
+//     case 2:
+//     System.Console.WriteLine("noy");
+//     break;
+//     case 3:
+//     System.Console.WriteLine("noy");
+//     break;
+//     case 4:
+//     System.Console.WriteLine("noy");
+//     break;
+//     case 5:
+//     System.Console.WriteLine("noy");
+//     break;
+//     case 6:
+//     System.Console.WriteLine("yes");
+//     break;
+//     case 7:
+//     System.Console.WriteLine("yes");
+//     break;
+// }
+
+if(day == 6 || day == 7)
 {
-    case 1:
-    System.Console.WriteLine("noy");
-    break;
-    case 2:
-    System.Console.WriteLine("noy");
-    break;
-    case 3:
-    System.Console.WriteLine("noy");
-    break;
-    case 4:
-    System.Console.WriteLine("noy");
-    break;
-    case 5:
-    System.Console.WriteLine("noy");
-    break;
-    case 6:
-    System.Console.WriteLine("yes");
-    break;
-    case 7:
-    System.Console.WriteLine("yes");
-    break;
+    Console.WriteLine("yes");
+}
+if(day < 6)
+{
+    Console.WriteLine("noy");
 }
